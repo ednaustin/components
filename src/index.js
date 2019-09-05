be35e61e4124f8067a59868f125e5336971597ed
ednaustin <edramirez512@gmail.com>
2019-09-05 08:32:33 -0500

    created ComponentDetail

--- src/index.js
+++ src/index.js
@@ -1,55 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
+import CommentDetail from './CommentDetail';
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" src={faker.image.avatar()} />
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            Sam
-          </a>
-          <div className="metadata">
-            <span className="date"> Today at 7:00PM</span>
-          </div>
-          <div className="text">Nice post man!</div>
-        </div>
-      </div>
-
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" src={faker.image.avatar()} />
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            Sam
-          </a>
-          <div className="metadata">
-            <span className="date"> Today at 7:00PM</span>
-          </div>
-          <div className="text">Nice post man!</div>
-        </div>
-      </div>
-
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" src={faker.image.avatar()} />
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            Sam
-          </a>
-          <div className="metadata">
-            <span className="date"> Today at 7:00PM</span>
-          </div>
-          <div className="text">Nice post man!</div>
-        </div>
-      </div>
+      <CommentDetail
+        author="Sam"
+        postDate="Today at 4:50PM"
+        postText="Great post!"
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Rob"
+        postDate="Today at 1:30PM"
+        postText="Nice!!"
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Alice"
+        postDate="Yesterday at 9:20PM"
+        postText="Great job, man!"
+        avatar={faker.image.avatar()}
+      />
     </div>
+
   )
 };
 
