22099b89f94022605bb13f19a080feef14b5dd95
ednaustin <edramirez512@gmail.com>
2019-09-05 07:56:21 -0500

    added jaker js library

--- src/index.js
+++ src/index.js
@@ -1,8 +1,58 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
 
 const App = () => {
-  return <div>Hi there!</div>;
+  return (
+    <div className="ui container comments">
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" src={faker.image.avatar()} />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Sam
+          </a>
+          <div className="metadata">
+            <span className="date"> Today at 7:00PM</span>
+          </div>
+          <div className="text">Nice post man!</div>
+        </div>
+      </div>
+
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" src={faker.image.avatar()} />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Sam
+          </a>
+          <div className="metadata">
+            <span className="date"> Today at 7:00PM</span>
+          </div>
+          <div className="text">Nice post man!</div>
+        </div>
+      </div>
+
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" src={faker.image.avatar()} />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Sam
+          </a>
+          <div className="metadata">
+            <span className="date"> Today at 7:00PM</span>
+          </div>
+          <div className="text">Nice post man!</div>
+        </div>
+      </div>
+    </div>
+  )
 };
+
+
 
 ReactDOM.render(<App />, document.querySelector('#root'));
